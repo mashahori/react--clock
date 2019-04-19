3e5854ea72c915dcd3ab6886fc7c50ab23204702
mashahori <mashahori@users.noreply.github.com>
2019-04-19 14:12:26 +0300

    Updates

---
+++ precache-manifest.49556d78cabe738c157210d45f331efc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "112dc520a516fefef18e",
+    "url": "/react--clock/static/js/runtime~main.66e8279f.js"
+  },
+  {
+    "revision": "322348aa2d7afaa09ccc",
+    "url": "/react--clock/static/js/main.b54b354f.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react--clock/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "322348aa2d7afaa09ccc",
+    "url": "/react--clock/static/css/main.da1b5f70.chunk.css"
+  },
+  {
+    "revision": "f64e5e40c1f84b85b0853e548fe77b48",
+    "url": "/react--clock/index.html"
+  }
+];
